b47db61b2cd718ff4c0bb6d39ff802cefc394785
Ayoub Omari <ayoubomari1@outlook.fr>
2021-04-02 20:39:49 +0200

    recursive DFS maze generation

--- maze/mazeGenerator.py
+++ maze/mazeGenerator.py
@@ -1,59 +1,112 @@
-from random import seed, uniform
+import pygame
+import random
 
-# seed(100011)
+SCREEN_SIDE = 640
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
 
-def fill(maze, unit, minX, maxX, minY, maxY):
-	if (maxX-minX)/unit < 2 or (maxY-minY)/unit < 2:
-		return
-	# print(int(minX/unit), int(maxX/unit), int(minY/unit), int(maxY/unit))
-	# print(minX, maxX, minY, maxY)
-
-	splitAtX = uniform(minX+unit, maxX-unit)
-	splitAtY = uniform(minY+unit, maxY-unit)
-
-	x, y = int(splitAtX/unit), int(splitAtY/unit)
-	# print("x=",x, "y=", y)
-
-	holeHorizontal1 = uniform(minX, splitAtX-unit)
-	holeHorizontal2 = uniform(splitAtX+unit, maxX)
-
-	holeVertical1 = uniform(minY, splitAtY-unit)
-	holeVertical2 = uniform(splitAtY+unit, maxY)
-
-	# print(int(holeHorizontal1/unit), int(holeHorizontal2/unit))
-	# print(int(holeVertical1/unit), int(holeVertical2/unit))
-
-	for j in range(int(minX/unit), int(maxX/unit)+1):
-		if j != int(holeHorizontal1/unit) and j != int(holeHorizontal2/unit):
-			print(j)
-			maze[y][j] = 1
-
-	for i in range(int(minY/unit), int(maxY/unit)+1):
-		if i != int(holeVertical1/unit) and i!=int(holeVertical2/unit):
-			maze[i][x] = 1
-
-	print(maze)
-
-	fill(maze, unit, minX, splitAtX-unit, minY, splitAtY-unit)
-	fill(maze, unit, splitAtX+unit, maxX, minY, splitAtY-unit)
-	fill(maze, unit, minX, splitAtX-unit, splitAtY+unit, maxY)
-	fill(maze, unit, splitAtX+unit, maxX, splitAtY+unit, maxY)
-		
+screen = None
+UNIT_SIDE = None 
 
 
 
-
-def generateMaze(size):
-	minX, maxX = 0, 2
-	minY, maxY = 0, 2
-	unit = max(maxX-minX, maxY-minY) / size
-	N, M = int((maxX-minX)/unit), int((maxY-minY)/unit)
-
-	maze = [[0 for _ in range(N)] for _ in range(M)]
-	fill(maze, unit, minX, maxX-unit, minY, maxY-unit)
-	print(maze)
-	return maze
+# pygame.draw.line(screen, (0, 0, 255), (0, 0), (639, 479))
+# pygame.draw.line(screen, (0, 0, 255), (639, 0), (0, 479))
+# pygame.display.flip()
 
 
-generateMaze(8)
+def initGrid(N):
+	global screen, UNIT_SIDE
+	screen = pygame.display.set_mode((SCREEN_SIDE, SCREEN_SIDE))
+	UNIT_SIDE = int(SCREEN_SIDE/N)
+	screen.fill(BLACK)
 
+
+def connect(x1, y1, x2, y2):
+	"""
+	removes the wall between two cells and visit the new cell
+	"""
+	print(UNIT_SIDE)
+
+	# remove wall
+	if y1 == y2: # vertical wall between horizontally connected cells
+		wallAbscissa = max(x2, x1)
+		# pygame.draw.line(screen, WHITE, 
+		# 	(wallAbscissa*UNIT_SIDE-2, y1*UNIT_SIDE-2),
+		# 	(wallAbscissa*UNIT_SIDE-2, y1*(UNIT_SIDE+1)-2), width = 8)
+
+
+		print("line: ", (wallAbscissa*UNIT_SIDE, y1*UNIT_SIDE),
+			(wallAbscissa*UNIT_SIDE, (y1+1)*UNIT_SIDE))
+		pygame.draw.line(screen, WHITE, 
+			(wallAbscissa*UNIT_SIDE-1, y1*UNIT_SIDE+1),
+			(wallAbscissa*UNIT_SIDE-1, (y1+1)*UNIT_SIDE-2), width = 3)
+
+
+	elif x1 == x2:
+		wallOrdinate = max(y1, y2)
+		pygame.draw.line(screen, WHITE, 
+			(x1*UNIT_SIDE+1, wallOrdinate*UNIT_SIDE-1),
+			((x1+1)*UNIT_SIDE-2, wallOrdinate*UNIT_SIDE-1), width=3)
+
+	# visit new cell
+	print("filling", (x2*UNIT_SIDE+2, y2*UNIT_SIDE+2, UNIT_SIDE-2, UNIT_SIDE-2))
+	pygame.draw.rect(screen, WHITE, (x2*UNIT_SIDE+1, y2*UNIT_SIDE+1, UNIT_SIDE-2, UNIT_SIDE-2))
+
+	pygame.display.flip()
+
+
+
+
+def drawMaze(x, y, N, visited):
+	neighbors = [(-1,0),(0,-1),(1,0),(0,1)]
+	validNeighbors = []
+	for nx,ny in neighbors:
+		if x + nx < N and y + ny < N and x+nx>=0 and y+ny>=0:
+			validNeighbors.append((x+nx, y+ny))
+
+
+	validNeighbors = [n for n in validNeighbors if n not in visited]
+	print(x, y, validNeighbors, visited)
+
+	if not validNeighbors: return
+
+	while validNeighbors:
+		nx, ny = random.choice(validNeighbors)
+		connect(x, y, nx, ny)
+
+		visited.add((nx, ny))
+		drawMaze(nx, ny, N, visited)
+		validNeighbors = [n for n in validNeighbors if n not in visited]
+
+	return
+
+
+
+N = 32
+initGrid(N)
+
+pygame.draw.rect(screen, WHITE, (1, 1, UNIT_SIDE-2, UNIT_SIDE-2))
+
+drawMaze(0, 0, N, set([(0, 0)]))
+
+
+
+
+
+running = True
+while running:
+	event = pygame.event.poll()
+	if event.type == pygame.QUIT:
+		running = False
+
+"""
+ idea: start with a grid with walls everywhere, then do a randomized depth first search that opens the walls
+
+ Questions:
+	+ How do we represent the maze as a matrix for solving ?
+		-> Maybe for each cell store its connected neighbors 
+	+ If we proceed by removing walls, what is (max_y, min_y) for a vertical wall and (max_x, min_x) for horizontal
+		-> We can control the thikness of walls measured in units, this will be deduced by that
+
+"""
