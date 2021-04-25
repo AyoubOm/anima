4ce062e4f20d9b16fde66307544458c0219e9c53
Ayoub Omari <ayoubomari1@outlook.fr>
2021-04-25 20:31:38 +0200

    render with matplotlib instead of pygame

--- fluid/fluid.py
+++ fluid/fluid.py
@@ -1,33 +1,25 @@
-import pygame
 from typing import Dict, Tuple, List
 import random
 
-N = 64
+N = 40
 gridSide= (N+2)*(N+2)
 
 prevDensity = [0.0 for _ in range(gridSide)] # using flattened array: perhaps better cache locality
 density = [0.0 for _ in range(gridSide)]
+
+prevDensity2 = [0.0 for _ in range(gridSide)] # using flattened array: perhaps better cache locality
+density2 = [0.0 for _ in range(gridSide)]
+
 prevVelocityX = [0.0 for _ in range(gridSide)]
 velocityX = [0.0 for _ in range(gridSide)]
-prevVelocityY = [3.0 for _ in range(gridSide)]
+prevVelocityY = [0.0 for _ in range(gridSide)]
 velocityY = [0.0 for _ in range(gridSide)]
 
 dt = 0.1 #TODO: value ?
 
 NB_ITER = 3
 
-
-SCREEN_SIDE = 640
-screen = None
-CELL_SIDE = int(SCREEN_SIDE/N)
-
-
-
-
-def initGrid():
-	global screen, SCREEN_SIDE, CELL_SIDE
-	screen = pygame.display.set_mode((SCREEN_SIDE, SCREEN_SIDE))
-	screen.fill((0, 0, 0))
+array = None
 
 
 
@@ -50,7 +42,7 @@ def addSource(field: List[float], sources: Dict[Tuple[int, int], float]):
 def diffuse(prevField: List[float], field: List[float], rate: float, onX: bool = False, onY: bool = False):
 	global dt, NB_ITER
 
-	a = rate * dt# Note: maybe rate need to adapt to size of N
+	a = rate * dt # Note: maybe rate need to adapt to size of N
 	for _ in range(NB_ITER): # number of iterations gauss seidel
 		for i in range(1, N+1):
 			for j in range(1, N+1):
@@ -67,9 +59,10 @@ def diffuse(prevField: List[float], field: List[float], rate: float, onX: bool =
 def advect(prevField: List[float], field: List[float], velocityX: List[float], velocityY: List[float], onX: bool = False, onY: bool = False):
 	# Note: maybe dt need to depend on N
 	global dt
+	dt0 = dt*N
 	for i in range(1, N+1):
 		for j in range(1, N+1):
-			x, y = i - velocityX[index(i,j)]*dt*N, j - velocityY[index(i,j)]*dt*N
+			x, y = i - velocityX[index(i,j)]*dt0, j - velocityY[index(i,j)]*dt0
 			if x < 0.5: x = 0.5
 			elif x > N+0.5: x = N+0.5
 			if y < 0.5: y = 0.5
@@ -81,8 +74,6 @@ def advect(prevField: List[float], field: List[float], velocityX: List[float], v
 			s0 = (1-s1) # = i0+1 - x
 			t1 = (y-j0)
 			t0 = (1-t1)
-			# if i == (N+2)//2 and j == (N+2)//2:
-			# print(s0, s1, t1, t0)
 			field[index(i,j)] = s0*(t0*prevField[index(i0,j0)] + t1*prevField[index(i0,j1)]) + s1*(t0*prevField[index(i1,j0)] + t1*prevField[index(i1,j1)])
 
 	setBoundaries(field, onX, onY)
@@ -133,49 +124,57 @@ def project():
 
 
 def densityStep():
-	# densSources = {((N+2)//2, (N+2)//2): 2550, (2*(N+2)//3, (N+2)//3): 2550} #TODO: value ?
 	source1 = ((N+2)//2, (N+2)//2)
 	source2 = (2*(N+2)//3, (N+2)//3)
 
-	densSources = {}
-	for i in range(3):
-		densSources[(source1[0]+i, source1[1]+i)] = 2550
+	densSources, densSources2 = {}, {}
+	for i in range(1):
+		densSources[(source1[0]+i, source1[1]+i)] = 2000.0
 
-	for i in range(3):
-		densSources[(source2[0]+i, source2[1]+i)] = 2550
+	# for i in range(3):
+	# 	densSources2[(source2[0]+i, source2[1]+i)] = 2550.0
 
 	global velocityX, velocityY, density, prevDensity
+	global prevDensity2, density2
 
 	addSource(density, densSources)
 	density, prevDensity = prevDensity, density
-	diffuse(prevDensity, density, rate = 0.1) #TODO: value ?
+	diffuse(prevDensity, density, rate = 0.5) #TODO: value ?
 	density, prevDensity = prevDensity, density
 	advect(prevDensity, density, velocityX, velocityY)
+
+	# for i in range(1, N+1):
+	# 	for j in range(1, N+1):
+	# 		if density[index(i, j)] > 10:
+	# 			print(density[index(i, j)])
+
+	global array
+	array = [[density[index(i, j)] for j in range(1, N+1)] for i in range(1, N+1)]
+
+
+	# addSource(density2, densSources2)
+	# density2, prevDensity2 = prevDensity2, density2
+	# diffuse(prevDensity2, density2, rate = 0.01)
+	# density2, prevDensity2 = prevDensity2, density2
+	# advect(prevDensity2, density2, velocityX, velocityY)
 
 
 
 def velocityStep():
 	source1 = ((N+2)//2, (N+2)//2)
-	source2 = (2*(N+2)//3, (N+2)//3)
 
 	velSourceX, velSourceY = {}, {}
-	for i in range(6):
-		velSourceX[(source1[0]+i, source1[1]+i)] = 6
-		velSourceX[(source2[0]+i, source2[1]+i)] = -5
+	velSourceX[source1] = -2
+	# velSourceY[source1] = -2.5
 
-	for i in range(6):
-		velSourceY[(source1[0]+i, source1[1]+i)] = -3
-		velSourceY[(source2[0]+i, source2[1]+i)] = 5
-
-	# velSourceX = {: -3, : -5} #TODO: value ?
-	# velSourceY = {(2*(N+2)//3, (N+2)//3): 5} #TODO: value ?
 
 	global velocityX, prevVelocityX, velocityY, prevVelocityY
 	addSource(velocityX, velSourceX)
+	addSource(velocityY, velSourceY)
 	velocityX, prevVelocityX = prevVelocityX, velocityX
 	velocityY, prevVelocityY = prevVelocityY, velocityY
-	diffuse(prevVelocityX, velocityX, rate = 0.01, onX = True) #TODO: value ?
-	diffuse(prevVelocityY, velocityY, rate = 0.01, onY = True) #TODO: value ?
+	diffuse(prevVelocityX, velocityX, rate = 0.5, onX = True) #TODO: value ?
+	diffuse(prevVelocityY, velocityY, rate = 0.5, onY = True) #TODO: value ?
 	project()
 	velocityX, prevVelocityX = prevVelocityX, velocityX
 	velocityY, prevVelocityY = prevVelocityY, velocityY
@@ -183,41 +182,23 @@ def velocityStep():
 	advect(prevVelocityY, velocityY, prevVelocityX, prevVelocityY, onY = True)
 	project()
 
-	# for i in range(1, N+1):
-	# 	for j in range(1, N+1):
-	# 		print(velocityY[index(i, j)])
 
 
 
 
-def drawDensity():
-	global density, CELL_SIDE, screen
-	for i in range(1, N+1):
-		for j in range(1, N+1):
-			if density[index(i, j)] > 255:
-				color = (255, 255, 255)
-			else:
-				color = (density[index(i, j)], density[index(i, j)], density[index(i, j)])
-
-			# print(color)
-			pygame.draw.rect(screen, color, (j*CELL_SIDE, i*CELL_SIDE, CELL_SIDE, CELL_SIDE))
-	pygame.display.flip()
-
-	print("middle color: ", density[index((N+2)//2, (N+2)//2)])
+import matplotlib.pyplot as plt
+from matplotlib import animation
 
 
-
-initGrid()
-# prevDensity[index((N+2)//2, (N+2)//2)] = 255
-# density[index((N+2)//2, (N+2)//2)] = 0
-	
-
-
-while True:
-	event = pygame.event.poll()
-	if event.type == pygame.QUIT:
-		pygame.quit()
+def update(i):
 	velocityStep()
 	densityStep()
-	drawDensity()
+	im.set_array(array)
 
+fig = plt.figure()
+
+array = [[density[index(i, j)] for j in range(1, N+1)] for i in range(1, N+1)]
+im = plt.imshow(array, cmap='gray', vmax=255, interpolation='bilinear')
+
+anim = animation.FuncAnimation(fig, update, interval=0)
+plt.show()
